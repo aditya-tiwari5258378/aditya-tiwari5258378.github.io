48c324af7bdd2d311f7b9e2aeb1f1a770b539ec8
aditya-tiwari5258378 <aditya.tiwari378@gmail.com>
2020-06-30 21:38:14 +0530

    Updates

---
+++ precache-manifest.b37cf0e3464146912b312ee9570fc4e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7fd01ad2ff6f15c3fe79b7811571372",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b6d99a28b4688f9ec8f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "56155510d3d77a4f8e00",
+    "url": "/static/js/2.0d3048ea.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0d3048ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b6d99a28b4688f9ec8f",
+    "url": "/static/js/main.c8cc1cb5.chunk.js"
+  },
+  {
+    "revision": "7406775fe39c7a37b144",
+    "url": "/static/js/runtime-main.27444ec2.js"
+  }
+]);
